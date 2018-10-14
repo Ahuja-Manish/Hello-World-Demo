af5a6393f21bd2ce3a6bd9813c670ba8853a9d13
AhujaManish05 <37679442+AhujaManish05@users.noreply.github.com>
2018-10-14 14:59:06 +0530

    Test Project Commit

---
+++ TestProject/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+using Moq;
+namespace TestProject
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Sum()
+        {
+            int a = 1, b = 2;
+            int c = 3;
+            Assert.Equals(c, (a + b));
+
+        }
+    }
+}
